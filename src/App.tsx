9800ad4b6b4dc01ba8b370e2c405c3341667669b
tangxue <11183504@qq.com>
2021-10-10 22:22:37 +0800

    feat: 生命周期

--- src/App.tsx
+++ src/App.tsx
@@ -14,6 +14,8 @@ interface State {
 }
 
 class App extends React.Component<Props, State> {
+  // * 生命周期第一阶段：初始化
+  // 初始化组件 state
   constructor(props: Props) {
     super(props)
     this.state = {
@@ -21,9 +23,34 @@ class App extends React.Component<Props, State> {
     }
   }
 
+  // 在组件创建好dom元素以后，挂载进页面的时候调用，只会调用一次
   componentDidMount() {
-    fetch("https://jsonplaceholder.typicode.com/users")
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(res => res.json())
+      .then((data) => this.setState({ robotGallery: data }))
   }
+
+  // * 生命周期第二个阶段：更新
+  // 在组件接收一个新的 prop（更新后）时被调用
+  // 已经被废弃（会产生副作用）
+  // componentWillReceiveProps(nextProps: Readonly<Props>, nextContext: any) {}
+
+  // 组件初始化和组件更新的时候被调用
+  // static getDerivedStateFromProps(props, state)
+
+  // 判断是否更新，返回一个布尔值
+  // shouldComponentUpdate(nextProps, nextState, nextContext: any): boolean {
+  //  return nextState.name !== this.state.name
+  // }
+
+
+  // 组件更新后调用 组件发生更新，UI重新渲染整个函数会被调用
+  componentDidUpdate(prevProps: Readonly<Props>, prevState: Readonly<State>, snapshot?: any) {}
+
+  // * 生命周期第三个阶段：销毁
+  // 组件销毁后调用
+  // 这个函数可以回收各种监听以及事件，用来防止可能存在内存泄漏
+  componentWillUnmount() {}
 
   render() {
     return (
@@ -34,8 +61,8 @@ class App extends React.Component<Props, State> {
         </div>
         <ShoppingCart />
         <div className={styles.robotList}>
-          {robots.map(r => (
-            <Robot id={r.id} name={r.name} email={r.email} />
+          {this.state.robotGallery.map(r => (
+            <Robot key={r.id} id={r.id} name={r.name} email={r.email} />
           ))}
         </div>
       </div>
