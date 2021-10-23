8191694fc8de58f7619d34a9a0e1c44454ce0913
tangxue <11183504@qq.com>
2021-10-23 19:22:55 +0800

    feat: 自定义hokks

--- src/components/AddToCart.tsx
+++ src/components/AddToCart.tsx
@@ -24,3 +24,23 @@ export const withAddToCart = (ChildComponent: React.ComponentType<RobotProps>) =
     return <ChildComponent {...props as RobotProps} addToCart={addToCart} />
   }
 }
+
+
+// 自定义 hook
+export const useAddToCart = () => {
+  const setState = useContext(appSetStateContext)
+
+  const addToCart: AddToCartFn = (id, name) => {
+    if (setState) {
+      setState((state: any) => {
+        return {
+          ...state,
+          shoppingCart: {
+            items: [...state.shoppingCart.items, { id, name }]
+          }
+        }
+      })
+    }
+  }
+  return addToCart
+}
--- src/components/RobotDiscount.tsx
+++ src/components/RobotDiscount.tsx
@@ -1,13 +1,14 @@
 import React, { useContext } from 'react'
 import styles from './Robot.module.css'
 import { appContext, appSetStateContext } from '../AppState'
-import { withAddToCart } from './AddToCart'
+import { useAddToCart } from './AddToCart'
 import { RobotProps } from './Robots'
 
 // React.FC = functional component 函数组件的缩写，代表是函数式组件的接口
 // 接口接受一个泛型类型 P , P 就是 props; FC<P = {}> = FunctionComponent<P>;
-const RobotDiscount: React.FC<RobotProps> = ({ id, name, email, addToCart }) => {
+const RobotDiscount: React.FC<RobotProps> = ({ id, name, email }) => {
   const value = useContext(appContext)
+  const addToCart = useAddToCart()
   return (
     <div className={styles.cardContainer}>
       <img alt="robot" src={`https://robohash.org/${id}`} />
@@ -21,4 +22,4 @@ const RobotDiscount: React.FC<RobotProps> = ({ id, name, email, addToCart }) => 
   )
 }
 
-export default withAddToCart(RobotDiscount)
+export default RobotDiscount
